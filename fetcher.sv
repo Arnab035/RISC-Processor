a2264a62b926bce00e7fcea5d0cd27083e98296c
Arnab035 <arnabjyotikalita35@gmail.com>
2017-02-27 20:32:51 -0500

    Added functionalities

--- fetcher.sv
+++ fetcher.sv
@@ -1,4 +1,8 @@
 `include "Sysbus.defs"
+`include "decode1.sv"
+`include "registerFile.sv"
+`include "alu.sv"
+`include "writeback.sv"
 
 module fetcher
 #(
@@ -8,37 +12,96 @@ module fetcher
   input  clk,
   input  fetch_en,
   input  [BUS_DATA_WIDTH-1:0] data,
-  input count 
+  input count ,
+  input end_of_cycle,
+  
+  output [31:0] outIns
 );
  
-reg [63:0] memdata[15:0] ;  // 16 * 32 memory stores instructions
-
-logic access = 1;
+logic low = 1;
 reg [31:0] ins, outIns;
- 
-always_comb begin
-	if(count) begin
-		memdata[2*count - 2] = data[31:0];
-		memdata[2*count - 1] = data[63:32];
-	end
-end
-	
-always @ (posedge clk)
+
+wire [5:0] out_alu_control;
+wire [5:0] out1_alu_control;
+wire [4:0] out_addressA;
+wire [4:0] out_addressB;
+wire [4:0] out_addressC;
+wire [BUS_DATA_WIDTH-1 : 0] out_imm;
+wire out_muxB_control;
+wire write_en;
+wire [BUS_DATA_WIDTH-1 : 0] dataA;
+wire [BUS_DATA_WIDTH-1 : 0] dataB;
+wire [BUS_DATA_WIDTH-1 : 0] dataOut;
+wire [BUS_DATA_WIDTH-1 : 0] writeBack;
+wire call_for_print;
+
+always @ (posedge clk) 
 	if(fetch_en) begin
-		if(access) begin
-			ins <= memdata[2* count - 2];
-			access <= 0;
+		$display(data);
+		if(low) begin
+				ins <= data[31:0];
+				low <= 0;
 		end else begin
-			ins <= memdata[2 * count - 1]; 
-			access <= 1;
-		end
+				ins <= data[63:32];
+				low <= 1;
+			end
 	end
+	
+
 	
 assign outIns = ins;
 
-decode d (
-	.clk(clk)
-	.outIns(outIns)
+decode1 d (
+	.clk(clk),
+	.end_of_cycle(end_of_cycle),
+	.fetch_en(fetch_en),
+	.outIns(outIns),
+	.out_alu_control(out_alu_control),
+	.out_addressA(out_addressA),
+	.out_addressB(out_addressB),
+	.out_addressC(out_addressC),
+	.out_imm(out_imm),
+	.out_muxB_control(out_muxB_control)
+);
+
+registerFile rf (
+	.clk(clk),
+	.end_of_cycle(end_of_cycle),
+	.fetch_en(fetch_en),
+	.alu_control(out_alu_control),
+	.muxB_control(out_muxB_control),
+	.write_en(write_en),
+	.call_for_print(call_for_print),
+	.addressA(out_addressA),
+	.addressB(out_addressB),
+	.writeBack(writeBack),
+	.addressC(out_addressC),
+	.imm(out_imm),
+	.dataA(dataA),
+	.dataB(dataB),
+	.out_alu_control(out1_alu_control)
+);
+
+alu al (
+	.clk(clk),
+	.end_of_cycle(end_of_cycle),
+	.fetch_en(fetch_en),
+	.dataA(dataA),
+	.dataB(dataB),
+	.alu_control(out1_alu_control),
+	.dataOut(dataOut),
+	._aluOps(aluOps)
+);
+
+writeback wb (
+	.clk(clk),
+	.end_of_cycle(end_of_cycle),
+	.fetch_en(fetch_en),
+	.dataOut(dataOut),
+	.y(writeBack),
+	.out_write_en(write_en),
+	._aluOps(aluOps),
+	.call_for_print(call_for_print)
 );
 
 endmodule
