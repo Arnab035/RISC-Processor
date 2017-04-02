046df9dd6d1b614999f40eac8879a9c25bef9950
Arnab035 <arnabjyotikalita35@gmail.com>
2017-04-02 04:41:38 -0400

    Modified data memory to align with a pipelined version

--- data_memory.sv
+++ data_memory.sv
@@ -1,28 +1,53 @@
 
+// 4th stage of pipeline
+
 `include "Sysbus.defs"
 
-module data_memory(
-	input [63:0] address,
-	input [63:0] writeData,
+module data_memory
+# (
+	BUS_DATA_WIDTH = 64
+)
+(
+	input [5:0] inWriteRegister,   // which register number to be written
+	
+	input [BUS_DATA_WIDTH-1:0] addressOrAluData,
+	input [BUS_DATA_WIDTH-1:0] writeData,
+	input inMemOrReg,        // if memorreg is 0, read data from memory
 	input memRead,
 	input memWrite,
 	
-	output [63:0] readData
+	// other control signals to be defined for branch
+	
+	output [BUS_DATA_WIDTH-1:0] readData,
+	output [5:0] outWriteRegister,
+	output [BUS_DATA_WIDTH-1:0] outAluData,
+	output outMemOrReg
 );
 
-reg [63:0] data_mem[2^64:0]; 
+reg [BUS_DATA_WIDTH-1:0] data_mem[2^(BUS_DATA_WIDTH) - 1 :0]; 
+reg [BUS_DATA_WIDTH-1:0] aluData;
+reg [BUS_DATA_WIDTH-1:0] outReadData;
+reg [5:0] writeRegister;
 
-reg [63:0] outReadData;
+logic _memOrReg;
 
 always @ (posedge clk) begin
-	if(memRead) begin
-		outReadData <= data_mem[address];
-	end else if(memWrite) begin
-		data_mem[address] <= writeData;
+	if(memRead && !inMemOrReg) begin
+		outReadData <= data_mem[addressOrAluData];
+	end else if(memWrite && !inMemOrReg) begin
+		data_mem[addressOrAluData] <= writeData;
+	end else if(inMemOrReg) begin
+		aluData <= addressOrAluData;
 	end
+	writeRegister <= inWriteRegister;
+	_memOrReg <= inMemOrReg;
 end
 
+
 assign readData = outReadData;
+assign outAluData = aluData;
+assign outWriteRegister = writeRegister;
+assign outMemOrReg = _memOrReg;
 
 endmodule
 
