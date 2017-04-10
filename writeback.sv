10c8fcb112c9418d52b70c81c80707ab19bfdf87
Arnab035 <arnabjyotikalita35@gmail.com>
2017-04-10 03:37:27 -0400

    Modify writeback module

--- writeback.sv
+++ writeback.sv
@@ -8,11 +8,15 @@ module writeback
 	BUS_DATA_WIDTH = 64
 )
 (
-	input inMemOrReg;
-	input [BUS_DATA_WIDTH-1 : 0] inReadData;     // read data from memory line
-	input [BUS_DATA_WIDTH-1 : 0] inALUData;      // data from ALU computation
+	input inMemOrReg,
+	input [BUS_DATA_WIDTH-1 : 0] inReadData,     // read data from memory line
+	input [BUS_DATA_WIDTH-1 : 0] inALUData,     // data from ALU computation
+	input [4:0] inDestReg,
+	input inRegWrite,
 	
-	output [BUS_DATA_WIDTH-1:0] outMemOrRegData;
+	output [BUS_DATA_WIDTH-1:0] outMemOrRegData,
+	output [4:0] outDestReg,
+	output outRegWrite                              // these 2 signals (the one above) must connect to the decoder module
 )
 
 always_comb begin
@@ -21,5 +25,7 @@ always_comb begin
 	end else begin
 		outMemOrRegData = inReadData;
 	end
+	outDestReg = inDestReg;         
+	outRegWrite = regWrite;
 end
 
